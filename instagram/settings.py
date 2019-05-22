53620e7083ba674b83cfe5761d6db97c9e065302
santa <ryancurtis998@yahoo.com>
2019-05-22 09:44:24 +0300

    initial commit

--- instagram/settings.py
+++ instagram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = 'i#6+to=rd!zjxt8^4s7)2!f#f&6kiol_-^21a=@%%klg#3gv1n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 
 # Application definition
